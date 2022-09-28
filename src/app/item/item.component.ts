f4b61e5f84ca4443b9cab349fffe19e471aa0b92
mbodenchak <mbodenchak@outlook.com>
2022-09-28 15:09:39 -0700

    create assignedAttendance emit

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -7,8 +7,19 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() student;
-
+  @Output() assignedAttendance = new EventEmitter<{
+    name: string;
+    attendance: string;
+  }>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  onAssign(attendance) {
+    // this.student.attendance = attendance;
+    this.assignedAttendance.emit({
+      name: this.student.name,
+      attendance: attendance,
+    });
+  }
 }
