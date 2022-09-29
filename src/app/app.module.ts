a61184da7fbb5e449add4ffda9d9ec1476efffd1
mbodenchak <mbodenchak@outlook.com>
2022-09-29 07:11:20 -0700

    implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
 import { ListComponent } from './list/list.component';
@@ -8,6 +9,18 @@ import { ItemComponent } from './item/item.component';
 import { AttendanceService } from './attendance.service';
 import { LogService } from './log.service';
 import { AddStudentComponent } from './add-student/add-student.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsComponent,
+  },
+  {
+    path: 'new-student',
+    component: AddStudentComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -16,8 +29,9 @@ import { AddStudentComponent } from './add-student/add-student.component';
     ListComponent,
     ItemComponent,
     AddStudentComponent,
+    HeaderComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [AttendanceService, LogService],
   bootstrap: [AppComponent],
 })
